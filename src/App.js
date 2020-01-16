aaf5514ba0a241d11550e8d6c8829b2faf2e6a5b
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-01-15 18:27:06 -0800

    added todo array, state, and TodoForm and TodoList in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
 
+const todo = [
+  {
+    task: 'Water Plants',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Exercise',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Bake Muffins',
+    id: 3,
+    completed: false
+  },
+  {
+    task: 'Study',
+    id: 4,
+    completed: false
+  },
+  {
+    task: 'Make Jewelry',
+    id: 5,
+    completed: false
+  }
+]
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todoList: todo
+    }
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="App">
+        <div className="header">
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm />
+        </div>
+        <TodoList />
       </div>
     );
   }
